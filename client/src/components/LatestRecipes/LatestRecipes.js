93d421b08b3d5b51e3ea1e5ecad8369f662169c2
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:40:30 +0200

    Create latest recipes componet

---
+++ client/src/components/LatestRecipes/LatestRecipes.js
@@ -0,0 +1,162 @@
+// import './Header.css';
+
+export default function LatestRecipes() {
+    return (
+        <div className="main-container">
+            <div className="inside-container">
+                <div className="row">
+                    <div className="col-12 our-header prom-padd">
+                        <h2>latest recipes</h2>
+                        <div className="text-center">
+                            <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                        </div>
+
+                        <div className="linker-work js-filter button">
+                            <button data-filter="*" >All Projects</button>   <button data-filter=".business"> Business Card</button>   <button data-filter=".postcard"> Post Cards</button>   <button data-filter=".brocher"> Brouchers</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row grid-gallery">
+                <div className="col-12">
+                    <div className="row gallery-height">
+                        <div className="col-lg-3 col-1-height col1-height col-over gallery-item brocher">
+                            <div className="col-overlay padd-col  ">
+                                <div className="our-header">
+                                    <p>Brouchers</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="row ">
+                                <div className=" col-lg-6  col2-height col-over gallery-item business">
+                                    <div className="col-overlay padd-col ">
+                                        <div className="our-header">
+                                            <p>BUSINESS CARD</p>
+                                            <div className="icon-overlay">
+                                                <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                                <a href="#">  <i className="fas fa-link"></i> </a>
+                                            </div>
+                                            <div className="text-center">
+                                                <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className=" col-lg-6 col2-1-height col-over gallery-item business">
+                                    <div className="col-overlay  ">
+                                        <div className="our-header">
+                                            <p>BUSINESS CARD</p>
+                                            <div className="icon-overlay">
+                                                <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                                <a href="#">  <i className="fas fa-link"></i> </a>
+                                            </div>
+                                            <div className="text-center">
+                                                <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-lg-12 col3-height col-over gallery-item business" >
+                                    <div className="col-overlay  ">
+                                        <div className="our-header">
+                                            <p>BUSINESS CARD</p>
+                                            <div className="icon-overlay">
+                                                <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                                <a href="#">  <i className="fas fa-link"></i> </a>
+                                            </div>
+                                            <div className="text-center">
+                                                <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-1-height col1-2-height col-over gallery-item brocher">
+                            <div className="col-overlay padd-col  ">
+                                <div className="our-header">
+                                    <p>Brouchers</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className=" col-lg-3 r3-col r3-col-1 col-over gallery-item postcard">
+                            <div className="col-overlay  ">
+                                <div className="our-header">
+                                    <p>POST CARDS</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" col-lg-3 r3-col r3-col-2 col-over gallery-item postcard">
+                            <div className="col-overlay  ">
+                                <div className="our-header">
+                                    <p>POST CARDS</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" col-lg-3 r3-col r3-col-3 col-over gallery-item postcard">
+                            <div className="col-overlay  ">
+                                <div className="our-header">
+                                    <p>POST CARDS</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 r3-col r3-col-4 col-over gallery-item postcard">
+                            <div className="col-overlay  ">
+                                <div className="our-header">
+                                    <p>POST CARDS</p>
+                                    <div className="icon-overlay">
+                                        <a href="#">  <i className="fas fa-compress-arrows-alt"></i></a>
+                                        <a href="#">  <i className="fas fa-link"></i> </a>
+                                    </div>
+                                    <div className="text-center">
+                                        <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
