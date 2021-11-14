47e0e81d7c682e9b47438a514d53e4a0973411e5
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:50:34 +0200

    Create constants folder for back-end server

---
+++ server/src/constants/index.js
@@ -0,0 +1,5 @@
+export const PORT = 3050;
+export const DB_URI = 'mongodb://localhost:27017/zahir';
+export const SALT_ROUNDS = 10;
+export const TOKEN_SECRET = 'FAF99AB68212CD88654D49C3B865C';
+export const COOKIE_NAME = 'authAppToken';
