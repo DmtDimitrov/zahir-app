35fdd84badffc4c61eb843feeb5c0a4525cb0135
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:44:58 +0200

    Update navigation menu
    add new buttons

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,6 +1,22 @@
 // import './Header.css';
 
-export default function Menu() {
+export default function Menu({
+    navigationHandler
+}) {
+    const onMenuClick = (e) => {
+        e.preventDefault();
+        // console.log('e.target.href');
+        // console.log(e.target.href);
+      
+        if (e.target.tagName == 'A') {
+            let url = new URL(e.target.href);
+
+            console.log('url');
+            console.log(url);
+            // console.log(url.pathname);
+            navigationHandler(url.pathname)
+        }
+    };
     return (
         <div className="main-container nav-bg-color">
             <div className="inside-container nav-contain">
@@ -10,16 +26,17 @@ export default function Menu() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav nav-list ml-auto">
-                            <a className="menu" href="index.html">HOME</a>
-                            <a className="menu" href="#">ABOUT US</a>
-                            <a className="menu" href="services.html">SERVICES</a>
-                            <a className="menu" href="#">BUSINESS CARD</a>
-                            <a className="menu" href="blog.html">BLOG</a>
-                            <a className="menu" href="blog-single.html">SINGLE POST</a>
-                            <a className="menu  last-spc" href="contact.html">CONTACT</a>
+                        <div onClick={onMenuClick} className="navbar-nav nav-list ml-auto">
+                            <a className="menu" href="/home">HOME</a>
+                            <a className="menu" href="/recipes">RECIPES</a>
+                            <a className="menu" href="/add">ADD RECIPE</a>
+                            <a className="menu" href="#">CHEFS</a>
+                            <a className="menu" href="/details">RECIPE</a>
+                            <a className="menu  last-spc" href="/contact">CONTACT</a>
                             <span className="menu last-spc color-line"> | </span>
-                            <a className="menu icon-color last-spc" href="#"><i className="fas fa-shopping-cart"></i></a>
+                            <a className="menu" href="/register">REGISTER</a>
+                            <a className="menu" href="/login">LOGIN</a>
+                            <a className="menu" href="#">LOGOUT</a>
                             <a className="menu icon-color  last-spc" href="#"><i className="fas fa-search"></i></a>
 
                         </div>
