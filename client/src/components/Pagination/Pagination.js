a62020db6021efeabe47b232fb21f8be01843cfb
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-15 01:07:34 +0200

    Create Pagination component

---
+++ client/src/components/Pagination/Pagination.js
@@ -0,0 +1,21 @@
+
+export default function Pagination() {
+
+    return (
+        <nav className="pagin-nr">
+            <ul className="pagination  justify-content-center">
+                <li className="page-item  arr-lf-rt">
+                    <a className="page-link" href="#" tabIndex="-1"><i className="fas fa-arrow-left"></i> </a>
+                </li>
+                <li className="page-item"><a className="page-link" href="#">01</a></li>
+                <li className="page-item active">
+                    <a className="page-link" href="#">02 <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                <li className="page-item arr-lf-rt">
+                    <a className="page-link" href="#"><i className="fas fa-arrow-right"></i></a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
