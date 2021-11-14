0a7b36e9618227664f57d64f0ff5ffbe80cb4d3e
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:45:58 +0200

    Adapt contact HTML to JSX

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,29 +1,36 @@
 import './Contact.css';
+import Subheader from '../../components/Subheader';
+
 
 export default function Contact() {
     return (
         <>
-            <div class="main-container ">
-                <div class="inside-container">
-                    <div class="row">
-                        <div class="col-sm-4 contact-col-marg text-center">
-                            <div class="row ">
-                                <div class="col-12 "><i class="fas fa-map-marker-alt col-icon-contact"></i></div>
+
+            <Subheader
+                title="Contact us"
+            />
+
+            <div className="main-container ">
+                <div className="inside-container">
+                    <div className="row">
+                        <div className="col-sm-4 contact-col-marg text-center">
+                            <div className="row ">
+                                <div className="col-12 "><i className="fas fa-map-marker-alt col-icon-contact"></i></div>
                             </div>
-                            <div class="row ">
-                                <div class="col-12 ">
-                                    <p> Collins Street West,<br/> 121 King Street, Melbourne.</p>
+                            <div className="row ">
+                                <div className="col-12 ">
+                                    <p> Collins Street West,<br /> 121 King Street, Melbourne.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-4 contact-col-marg-middle contact-col-border text-center">
-                            <div class="row ">
-                                <div class="col-12 "> <i class="fas fa-phone fa-rotate-90 col-icon-contact"></i></div>
+                        <div className="col-sm-4 contact-col-marg-middle contact-col-border text-center">
+                            <div className="row ">
+                                <div className="col-12 "> <i className="fas fa-phone fa-rotate-90 col-icon-contact"></i></div>
 
                             </div>
-                            <div class="row ">
-                                <div class="col-12 ">
-                                    <p>+(11)123 456 7890<br/>
+                            <div className="row ">
+                                <div className="col-12 ">
+                                    <p>+(11)123 456 7890<br />
                                         +(11)123 456 7890 </p>
                                 </div>
 
@@ -31,13 +38,13 @@ export default function Contact() {
 
                         </div>
 
-                        <div class="col-sm-4 contact-col-marg text-center">
-                            <div class="row ">
-                                <div class=" col-12 "> <i class="far fa-envelope col-icon-contact "> </i></div>
+                        <div className="col-sm-4 contact-col-marg text-center">
+                            <div className="row ">
+                                <div className=" col-12 "> <i className="far fa-envelope col-icon-contact "> </i></div>
                             </div>
-                            <div class="row ">
-                                <div class="col-12 ">
-                                    <p>info@flamix.com<br/>info @flamix.com</p>
+                            <div className="row ">
+                                <div className="col-12 ">
+                                    <p>info@flamix.com<br />info @flamix.com</p>
                                 </div>
                             </div>
 
@@ -45,44 +52,44 @@ export default function Contact() {
                     </div>
                 </div>
             </div>
-            <div class="main-container ">
+            <div className="main-container ">
                 <div id="maps">
 
                 </div>
 
             </div>
-           
-            <div class="main-container">
-                <div class="inside-container">
-                    <div class="row">
-                        <div class="col-md-12 ">
-                            <div class="">
-                                <div class="our-header">
+
+            <div className="main-container">
+                <div className="inside-container">
+                    <div className="row">
+                        <div className="col-md-12 ">
+                            <div className="">
+                                <div className="our-header">
                                     <h2>get in touch with us</h2>
-                                    <div class="text-center">
-                                        <hr/> <i class ="far fa-square rotate-45"></i> <i class ="far fa-square rotate-45"></i>
-                                        <hr/>
-                                        </div>
+                                    <div className="text-center">
+                                        <hr /> <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i>
+                                        <hr />
                                     </div>
-
-
-                                    <form action="#">
-                                        <div class="row">
-                                            <div class="col-sm-4"> <input type="text" placeholder="Your Name*" class="sm-input"/>
-                                            </div>
-                                            <div class="col-sm-4"> <input type="email" placeholder="Your Email*" class="sm-input"/>
-                                            </div>
-                                            <div class="col-sm-4"> <input type="text" placeholder="Website" class="sm-input"/></div>
-                                        </div>
-                                        <input type="text" placeholder="Subject" class="sb-input"/>
-                                        <textarea rows="6" class ="area-text" placeholder="Type Your Mesage Here..."></textarea>
-                                        <div class ="text-center"> <input type ="submit" value="send mesage" class ="submit-btn"/></div>
-                                    </form>
                                 </div>
+
+
+                                <form action="#">
+                                    <div className="row">
+                                        <div className="col-sm-4"> <input type="text" placeholder="Your Name*" className="sm-input" />
+                                        </div>
+                                        <div className="col-sm-4"> <input type="email" placeholder="Your Email*" className="sm-input" />
+                                        </div>
+                                        <div className="col-sm-4"> <input type="text" placeholder="Website" className="sm-input" /></div>
+                                    </div>
+                                    <input type="text" placeholder="Subject" className="sb-input" />
+                                    <textarea rows="6" className="area-text" placeholder="Type Your Mesage Here..."></textarea>
+                                    <div className="text-center"> <input type="submit" value="send mesage" className="submit-btn" /></div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </>
-            );
+            </div>
+        </>
+    );
 }
