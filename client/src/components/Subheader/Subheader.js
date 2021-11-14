8eddbff4b806b9611701ca74b3c2c3bfe6f7369f
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:44:10 +0200

    Update Subheader.js

--- client/src/components/Subheader/Subheader.js
+++ client/src/components/Subheader/Subheader.js
@@ -1,30 +1,21 @@
 // import './Header.css';
 
-export default function Subheader() {
+export default function Subheader({
+    title
+}) {
     return (
-        <div className="main-container nav-bg-color">
-            <div className="inside-container nav-contain">
-                <nav className="navbar navbar-expand-lg navbar-light ">
-                    <img src="zahir/zahir3.png" className="img-fluid" alt="" />
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav nav-list ml-auto">
-                            <a className="menu" href="index.html">HOME</a>
-                            <a className="menu" href="#">ABOUT US</a>
-                            <a className="menu" href="services.html">SERVICES</a>
-                            <a className="menu" href="#">BUSINESS CARD</a>
-                            <a className="menu" href="blog.html">BLOG</a>
-                            <a className="menu" href="blog-single.html">SINGLE POST</a>
-                            <a className="menu  last-spc" href="contact.html">CONTACT</a>
-                            <span className="menu last-spc color-line"> | </span>
-                            <a className="menu icon-color last-spc" href="#"><i className="fas fa-shopping-cart"></i></a>
-                            <a className="menu icon-color  last-spc" href="#"><i className="fas fa-search"></i></a>
-
-                        </div>
+        <div className="main-container connect">
+            <div className="inside-container">
+                <div className="row">
+                    <div className="col-12 subhead">
+                        <h1 className="text-center">{title}</h1>
+                        <nav className="breadcrumb page-linker">
+                            <a className="breadcrumb-item" href="#">Home <i className="fas fa-angle-right"></i></a>
+                            <span className="breadcrumb-item active">{title}</span>
+                        </nav>
                     </div>
-                </nav>
+
+                </div>
             </div>
         </div>
     );
