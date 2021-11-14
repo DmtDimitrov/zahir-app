9c8f031eed6c79e41164d2c09950bc801e0efe2a
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-14 23:51:33 +0200

    Create user service

---
+++ server/src/services/userService.js
@@ -0,0 +1,38 @@
+import User from '../models/User.js'
+import { createToken } from './tokenService.js';
+
+export async function register(userData) {
+    try {
+        await User.create(userData);
+    } catch (error) {
+        throw error;
+    }
+};
+
+export async function login({ email, password }) {
+   
+    try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+			throw 'Invalid email or password';
+		}
+
+        let isValid = await user.validatePassword(password);
+
+        if (!isValid) {
+			throw 'Invalid email or password';
+		}
+
+        let { accessToken, refreshToken } = await createToken(user);
+
+        user.refreshToken = refreshToken;
+
+        await user.save();
+
+        return { user, accessToken, refreshToken };
+    } catch (error) {
+        throw error;
+    }
+};
+
