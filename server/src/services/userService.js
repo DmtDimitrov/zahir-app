b67b5ef6c6cc9cc09f8e9139c4b69ebe723b5c29
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-15 03:33:32 +0200

    Fix some submissionс of variables

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,33 +1,34 @@
 import User from '../models/User.js';
 import { createToken } from './tokenService.js';
 
-export async function register(userData) {
+export async function register({ firstName, lastName, email, password }) {
     try {
-        await User.create(userData);
+        return await User.create({ firstName, lastName, email, password });
     } catch (error) {
-        throw error;
+        throw new Error(error);
     }
 };
 
 export async function login({ email, password }) {
-   
+
     try {
         let user = await User.findOne({ email });
-
+        console.log('login user');
+        console.log(user);
         if (!user) {
-			throw 'Invalid email or password';
-		}
+            throw 'Invalid email or password';
+        }
 
         let isValid = await user.validatePassword(password);
 
         if (!isValid) {
-			throw 'Invalid email or password';
-		}
+            throw 'Invalid email or password';
+        }
 
         let { accessToken, refreshToken } = await createToken(user);
 
         user.refreshToken = refreshToken;
-
+       
         await user.save();
 
         return { user, accessToken, refreshToken };
