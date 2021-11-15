de36e350ad5b1cf390599c0a39f6a62cb092dddf
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-15 04:35:40 +0200

    Update recipeController.js
    create post recipe

--- server/src/controllers/recipeController.js
+++ server/src/controllers/recipeController.js
@@ -8,9 +8,9 @@ router.get('/', async (req, res) => {
 
     console.log(req.body);
 
-   
     try {
-        
+        let recipe = await recipeService.getAll();
+        res.json(recipe);
     } catch (error) {
         res.json({
             type: 'error',
@@ -21,7 +21,21 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    console.log('req.body');
+    console.log(req.body);
+    let userId = '6191b1b8015eda7d3c742fd7'
+   
+    try {
+        
+        await recipeService.create({...req.body, author: userId});
     
+        res.json({ok: true});
+    } catch (error) {
+        res.json({
+            type: 'error',
+            message: error.message
+        })
+    }
 })
 
 export default router;
