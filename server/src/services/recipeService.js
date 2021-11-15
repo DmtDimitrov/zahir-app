c3b0d4eec5739b9e10fc447d913dfe2da6db48b6
Dimitar Dimitrov <dmt.dimitrov@gmail.com>
2021-11-15 19:19:14 +0200

    Update recipeService.js

--- server/src/services/recipeService.js
+++ server/src/services/recipeService.js
@@ -1,7 +1,7 @@
 import Recipe from '../models/Recipe.js';
 import User from '../models/User.js';
 
-export const getAll = () => Recipe.find().populate('author');
+export const getAll = () => Recipe.find();
 export const getOne = (id) => Recipe.findById(id);
 export const create = (recipeData) => Recipe.create(recipeData);
 export const update = (id, recipeData) => Recipe.findByIdAndUpdate(id, recipeData);
