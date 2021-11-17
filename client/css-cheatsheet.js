8e4df9cce5678fdebf079b83b6fd238d872abe8d
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-18 01:35:29 +0200

    Update css-cheatsheet.js

--- client/css-cheatsheet.js
+++ client/css-cheatsheet.js
@@ -1,14 +1,14 @@
-{`${styles['breadcrumb-item']} ${styles.active}`}
-{`${styles['dream-text']} ${styles['p']}`}
-{`row ${styles.myCustomClassName}`}
-{`page-footer ${styles['footer-bg']}`}
+// {`${styles['breadcrumb-item']} ${styles.active}`}
+// {`${styles['dream-text']} ${styles['p']}`}
+// {`row ${styles.myCustomClassName}`}
+// {`page-footer ${styles['footer-bg']}`}
 
-{`col-12 ${styles.subhead}`}
-{`${styles['breadcrumb-item']} ${styles.active}`}
-{`${styles['main-container']} ${styles.subhead}`}
-{`${styles.breadcrumb} ${styles['page-linker']}`}
-{`${styles.subhead} ${styles['main-container']}`}
-{`row ${styles.myCustomClassName}`}
-{`col-12 ${styles['copyright-text']}`}
-{styles['pop-col']}
-{styles['breadcrumb-item']}
+// {`col-12 ${styles.subhead}`}
+// {`${styles['breadcrumb-item']} ${styles.active}`}
+// {`${styles['main-container']} ${styles.subhead}`}
+// {`${styles.breadcrumb} ${styles['page-linker']}`}
+// {`${styles.subhead} ${styles['main-container']}`}
+// {`row ${styles.myCustomClassName}`}
+// {`col-12 ${styles['copyright-text']}`}
+// {styles['pop-col']}
+// {styles['breadcrumb-item']}
