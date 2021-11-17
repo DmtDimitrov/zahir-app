8bfc481cc86057c11b1ff455bae9435e98962a88
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-18 01:33:31 +0200

    Setup footer css module

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,82 +1,63 @@
-// import './Header.css';
+import styles from './Footer.module.css';
+
 
 export default function Footer() {
     return (
-        <footer className="page-footer footer-bg">
-        <div className="inside-container">
-            <div className="row footer-padd">
-                <div className=" col-sm-6 col-lg-3 foot-col-padd">
-                    <div className="foot-logo"><img src="zahir/zahir4.png" alt="" className="img-fluid"/></div>
-                    <div className="dream-text">
-                        <p>Got a dream and we just know now we're gonna make our dream come true. No phone no lights no
-                            motor car not a single luxury. </p>
+        <footer className={`page-footer ${styles['footer-bg']}`}>
+            <div className={styles['inside-container']}>
+                <div className={`row ${styles['footer-padd']}`}>
+                    <div className={`col-sm-6 col-lg-3 ${styles['foot-col-padd']}`}>
+                        <div className={styles['foot-logo']}><img src="zahir/zahir4.png" alt="" className="img-fluid" /></div>
+                        <div className={styles['dream-text']}>
+                            <p className={`${styles['dream-text']} ${styles['p']}`}>Our motto is also our name, namely "Zahir - something we have touched or seen once and can never forget again, filling our thoughts to such an extent that it drives us crazy." </p>
+                        </div>
+                        <div>
+                            <i className={`fab fa-facebook-f ${styles['foot-icon']}`}></i>
+                            <i className={`fab fa-twitter ${styles['foot-icon']}`}></i>
+                            <i className={`fab fa-linkedin-in ${styles['foot-icon']}`}></i>
+                            <i className={`fab fa-tumblr ${styles['foot-icon']}`}></i>
+                            <i className={`fab fa-vimeo-v ${styles['foot-icon']}`}></i>
+                            <i className={`fab fa-pinterest-p ${styles['foot-icon']}`}></i>
+                        </div>
                     </div>
-                    <div>
-                        <i className="fab fa-facebook-f foot-icon "></i>
-                        <i className="fab fa-twitter foot-icon "></i>
-                        <i className="fab fa-linkedin-in foot-icon "></i>
-                        <i className="fab fa-tumblr  foot-icon"></i>
-                        <i className="fab fa-vimeo-v  foot-icon"></i>
-                        <i className="fab fa-pinterest-p foot-icon "></i>
+                    <div className={`col-sm-6 col-lg-3 ${styles['pop-col']}`}>
+                        <span>popular link</span>
+                        <hr />
+                        <div className="row">
+                            <div className={`col-6 ${styles['pop-link']}`}>
+                                <a href="#">About Us</a>
+                                <a href="#">contact us </a>
+                                <a href="#">recipes</a>
+                                <a href="#">Chefs</a>
+                            </div>
+    
+                        </div>
                     </div>
-                </div>
-                <div className=" col-sm-6 col-lg-3 pop-col">
-                    <span>popular link</span>
-                    <hr/>
-                    <div className="row">
-                        <div className="col-6 pop-link">
-                            <a href="#">About Us</a>
-                            <a href="#">Services</a>
-                            <a href="#">Gallery</a>
-                            <a href="#">Team</a>
-                            <a href="#">Pricing</a>
 
+                    <div className={`col-sm-6 col-lg-3 ${styles['pop-col']}`}>
+                        <span>contact us</span>
+                        <hr />
+                        <div className={`row ${styles['contact-row-margin']}`}>
+                            <i className={`fas fa-map-marker-alt ${styles['contact-icon']}`}></i>
+                            <p> 52 Petar Parchevich Street,<br /> Sofia.</p>
                         </div>
-                        <div className=" col-6  pop-link">
-                            <a href="#">contact us </a>
-                            <a href="#"> blog</a>
-                            <a href="#"> faq</a>
-                            <a href="#"> 404</a>
+                        <div className={`row ${styles['contact-row-margin']}`}>
+                            <i className={`fas fa-phone fa-rotate-90 ${styles['contact-icon']}`}></i>
+                            <p>+359 879 213 566</p>
+                        </div>
+                        <div className={`row ${styles['contact-row-margin']}`}>
+                            <i className={`far fa-envelope ${styles['contact-icon']}`}> </i>
+                            <p> info@zahirsweetcreations.com </p>
                         </div>
                     </div>
                 </div>
-                <div className="col-sm-6  col-lg-3 pop-col">
-                    <span>recent news </span>
-                    <hr/>
-                    <div className="row ltl-blog row-ltl-blog">
-                        <div className="col-3"> <img src="blog/recent-post-1.jpg" alt="" className="blog-img"/></div>
-                        <div className="col-9 max-award">
-                            <p> Max Printshop wins an award</p>
-                            <i className="far fa-heart blog-icon"></i> <span> 45 </span> <span className="blog-vl"> | </span>
-                            <i className="far fa-comment blog-icon"></i><span> 34 </span>
-                        </div>
-                    </div>
-                    <hr className="recent-hr"/>
-                    <div className="text-center"><a href="#" className="view-blog"> view blog</a> </div>
-                </div>
-                <div className="col-sm-6 col-lg-3 pop-col ">
-                    <span>contact us</span>
-                    <hr/>
-                    <div className="row contact-row-margin">
-                        <i className="fas fa-map-marker-alt contact-icon"></i>
-                        <p> Collins Street West,<br/> King Street, Melbourne.</p>
-                    </div>
-                    <div className="row contact-row-margin">
-                        <i className="fas fa-phone fa-rotate-90 contact-icon"></i>
-                        <p>+(11)123 456 789</p>
-                    </div>
-                    <div className="row contact-row-margin">
-                        <i className="far fa-envelope contact-icon "> </i>
-                        <p> info@flamix.com </p>
+                <div className="row">
+                    <div className={`col-12 ${styles['copyright-text']}`}>
+                        <p> &#169; 2019 <a href="http://digitaconnect.com/">Digital Connect</a>. All Rights Reserved </p>
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 copyright-text">
-                    <p> &#169; 2019 <a href="http://digitaconnect.com/">Digital Connect</a>. All Rights Reserved </p>
-                </div>
-            </div>
-        </div>
-    </footer>
+        </footer>
     );
 }
+
