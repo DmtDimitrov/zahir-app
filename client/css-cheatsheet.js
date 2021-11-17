083c17ca6c01d06c4e5c2469a7753b05915d328e
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-18 00:14:42 +0200

    Create css-module cheatsheet

---
+++ client/css-cheatsheet.js
@@ -0,0 +1,8 @@
+{`col-12 ${styles.subhead}`}
+{`${styles['breadcrumb-item']} ${styles.active}`}
+{`${styles['main-container']} ${styles.subhead}`}
+{`${styles.breadcrumb} ${styles['page-linker']}`}
+{`${styles.subhead} ${styles['main-container']}`}
+{`row ${styles.myCustomClassName}`}
+{styles['text-center']}
+{styles['breadcrumb-item']}
