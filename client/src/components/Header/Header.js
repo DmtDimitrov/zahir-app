18bf8430d15095901f9288b0667d517b48a4644b
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-18 02:30:03 +0200

    Setup header css modules

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,25 +1,25 @@
-// import './Header.css';
+import styles from './Header.module.css';
 
 export default function Header() {
     return (
-        <div className="main-container contact-color">
-            <div className="inside-container ">
-                <div className="row row-spc">
-                    <div className="col-md-12  col-disp">
-                        <div className="icon-scp" >
-                            <div className="left-side">
+        <div className={`${styles['main-container']} ${styles['contact-color']}`}>
+            <div className={styles['inside-container']}>
+                <div className={`row ${styles['row-spc']}`}>
+                    <div className={`col-md-12 ${styles['col-disp']}`}>
+                        <div className={styles['icon-scp']} >
+                            <div className={styles['left-side']}>
                                 <a className="link-head " href="#"><i className="fas fa-phone fa-rotate-90"></i>  +(11)123 456 7890</a> <span>|</span>
                                 <a className="link-head " href="#"><i className="far fa-clock"></i> Sun-Mon: 9.00am to 7.00pm</a> <span>|</span>
-                                <a className="link-head " href="#"><i className="far fa-envelope "> </i> info@flamix.com </a>
+                                <a className={styles['link-head']} href="#"><i className="far fa-envelope "> </i> info@flamix.com </a>
                             </div>
                         </div>
-                        <div className="ml-auto icon-scp" >
+                        <div className={`ml-auto ${styles['icon-scp']}`} >
                             <i className="fab fa-facebook-f "></i>
-                            <i className="fab fa-twitter icon-scp"></i>
-                            <i className="fab fa-linkedin-in icon-scp"></i>
-                            <i className="fab fa-tumblr icon-scp"></i>
-                            <i className="fab fa-vimeo-v icon-scp"></i>
-                            <i className="fab fa-pinterest-p icon-scp"></i>
+                            <i className={`fab fa-twitter ${styles['icon-scp']}`}></i>
+                            <i className={`fab fa-linkedin-in ${styles['icon-scp']}`}></i>
+                            <i className={`fab fa-tumblr ${styles['icon-scp']}`}></i>
+                            <i className={`fab fa-vimeo-v ${styles['icon-scp']}`}></i>
+                            <i className={`fab fa-pinterest-p ${styles['icon-scp']}`}></i>
                         </div>
                     </div>
                 </div>
