da71d7254ebaba19c5afd767578508775dc4f499
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-18 11:51:15 +0200

    setup header css module

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,13 +8,13 @@ export default function Header() {
                     <div className={`col-md-12 ${styles['col-disp']}`}>
                         <div className={styles['icon-scp']} >
                             <div className={styles['left-side']}>
-                                <a className="link-head " href="#"><i className="fas fa-phone fa-rotate-90"></i>  +(11)123 456 7890</a> <span>|</span>
-                                <a className="link-head " href="#"><i className="far fa-clock"></i> Sun-Mon: 9.00am to 7.00pm</a> <span>|</span>
+                                <a className={styles['link-head']} href="#"><i className="fas fa-phone fa-rotate-90"></i>  +(11)123 456 7890</a> <span>|</span>
+                                <a className={styles['link-head']} href="#"><i className="far fa-clock"></i> Sun-Mon: 9.00am to 7.00pm</a> <span>|</span>
                                 <a className={styles['link-head']} href="#"><i className="far fa-envelope "> </i> info@flamix.com </a>
                             </div>
                         </div>
                         <div className={`ml-auto ${styles['icon-scp']}`} >
-                            <i className="fab fa-facebook-f "></i>
+                            <i className={`fab fa-facebook-f `}></i>
                             <i className={`fab fa-twitter ${styles['icon-scp']}`}></i>
                             <i className={`fab fa-linkedin-in ${styles['icon-scp']}`}></i>
                             <i className={`fab fa-tumblr ${styles['icon-scp']}`}></i>
