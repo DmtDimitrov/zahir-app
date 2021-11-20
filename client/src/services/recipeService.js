8af9f974ea361f9e3e04975c3cec4ef558b98d60
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-20 03:23:58 +0200

    Update recipeService
    alternative getAll function with async await

--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -1,11 +1,17 @@
 import { HOST } from "../constants";
 
-export function getAll(){
+export function getAll() {
     return fetch(`${HOST}/recipes`)
         .then(res => res.json())
 };
 
-export function getAuthor(){
+// export async function getAll(){
+//     let response = await fetch(`${HOST}/recipes`);
+//     let recipes = response.json();
+//     return recipes;
+// };
+
+export function getAuthor() {
     return fetch(`${HOST}/recipes`)
         .then(res => res.json())
 };
