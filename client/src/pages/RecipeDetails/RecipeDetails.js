7e45ef339b280d7f859acc25bfe222a9fa5bb7c6
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-22 15:13:45 +0200

    Create PopularTag component and setup css module

--- client/src/pages/RecipeDetails/RecipeDetails.js
+++ client/src/pages/RecipeDetails/RecipeDetails.js
@@ -9,6 +9,7 @@ import Comments from '../../components/Comments';
 import AddComment from '../../components/Comments/AddComment';
 import SearchBar from '../../components/SearchBar';
 import CategoriesBar from '../../components/CategoriesBar';
+import PopularTags from '../../components/PopularTags';
 
 export default function RecipeDetails({
     match
@@ -138,28 +139,9 @@ export default function RecipeDetails({
                                 </div>
                             </div>
                             
+                        
+                            <PopularTags />
                            
-                            <div className="row blog-row-padd">
-                                <div className="col-12 side-bar-bg">
-                                    <div className="our-header text-center">
-                                        <span>popular tags</span>
-                                        <div className="text-center">
-                                            <hr /><i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i> <hr />
-                                        </div>
-                                    </div>
-                                    <div className="popular-link">
-                                        <button >About Us</button>
-                                        <button> Services </button>
-                                        <button>Gallery </button>
-                                        <button>Products </button>
-                                        <button>Business Cards</button>
-                                        <button>Brocher</button>
-                                        <button>Post Cards</button>
-                                    </div>
-
-
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
