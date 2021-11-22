c9daf80581217201cfb1f3522d38788c03bef78e
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-23 00:15:58 +0200

    Update RecipeDetails css and import RecipeDetailsCard

--- client/src/pages/RecipeDetails/RecipeDetails.js
+++ client/src/pages/RecipeDetails/RecipeDetails.js
@@ -10,6 +10,7 @@ import AddComment from '../../components/Comments/AddComment';
 import SearchBar from '../../components/SearchBar';
 import CategoriesBar from '../../components/CategoriesBar';
 import PopularTags from '../../components/PopularTags';
+import RecipeDetailsContentCard from './RecipeDetailsContentCard'
 
 export default function RecipeDetails({
     match
@@ -36,55 +37,23 @@ export default function RecipeDetails({
                 <div className={styles['inside-container']}>
                     <div className="row">
 
+                        <div className=" col-lg-12 " >
+
+                        
+
+                            <PopularTags
+                                title="Categories"
+                            />
+
+                        </div>
+                        
+
+
+
                         <div className=" col-lg-8 " >
 
-                            <div className="row">
-                                <div className="col-md-12 ">
-                                    <div className={styles['recipe-post']}>
-                                        <div> <img src={recipe.image} alt="" className={`${styles['recipe-image']} img-fluid`} /></div>
-                                        <div className={styles['recipe-description']}>
-                                            <div className="text-center">  <a href="#"> Shop </a>
-                                                <h1>{recipe.title}</h1>
-                                                <div className={styles['post-info']}> <p> <span className={styles['info-over']}>  <i className="far fa-user"></i> By Admin</span> <span className={styles['spc-line']}> |</span> <span className={styles['info-over']}> Dec 24,2016</span>
-                                                    <span className={styles['spc-line']}> |</span> <span className={styles['info-over']}> <i className="far fa-heart"></i> 39 Likes </span> <span className={styles['spc-line']}> |</span>
-                                                    <span className={styles['info-over']}> <i className="far fa-comment"></i> 55 comments </span> </p> </div>
-                                            </div>
 
-                                            <p> {recipe.description}</p>
-
-
-                                            <div className={styles['explore']}>
-                                                <p> Explore strange new worlds to seek out new life and new civilizations to boldly go where no man has gone before.
-                                                </p>
-                                                <i className="fas fa-quote-right"></i>
-                                            </div>
-                                            <p>
-                                                Sometimes you want to go where everybody knows your name. And they're always glad you came. Makin' your way in the world today takes everything you've got.
-                                                Takin' a break from all your worries sure would help a lot. Makin' your way in the world today takes everything you've got.
-                                            </p>
-                                            <div className="row">
-                                                <div className={`col-md-8 ${styles['btn-icon']}`}>
-                                                    <button>About us</button>
-                                                    <button>Services</button>
-                                                    <button>Gallery</button>
-                                                </div>
-                                                <div className=" col-md-4 ">
-                                                    <div className={`text-right ${styles['icon-btn']}`}>
-
-                                                        <i className="fab fa-facebook-f"></i>
-                                                        <i className="fab fa-twitter"></i>
-                                                        <i className="fab fa-linkedin-in"></i>
-                                                        <i className="fab fa-tumblr"></i>
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                            <hr />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+                            <RecipeDetailsContentCard {...recipe} />
 
                             <Comments />
 
@@ -100,7 +69,7 @@ export default function RecipeDetails({
 
                             <CategoriesBar />
 
-                           
+
                             <div className="row blog-row-padd">
                                 <div className="col-12 side-bar-bg">
                                     <div className="our-header text-center">
@@ -138,10 +107,12 @@ export default function RecipeDetails({
                                     <hr />
                                 </div>
                             </div>
-                            
-                        
-                            <PopularTags />
-                           
+
+
+                            <PopularTags
+                                title="Popular Tags"
+                            />
+
                         </div>
                     </div>
                 </div>
