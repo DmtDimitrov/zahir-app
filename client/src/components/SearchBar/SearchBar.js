b4e3d548e917dcd5bce6fac9669f51b77f130a68
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-24 04:48:17 +0200

    Setup css module at searchBar

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,21 @@
+import styles from './SearchBar.module.css';
+
 export default function SearchBar() {
 
     return (
         <div className="row">
-            <div className="col-12  side-bar-bg">
-                <div className="recipes-our-header text-center">
+            <div className={`col-12 ${styles['side-bar-bg']}`}>
+                <div className={`${styles['our-header']} text-center`}>
                     <span>Search</span>
                     <div className="text-center">
-                        <hr /> <i className="far fa-square rotate-45"></i> <i className="far fa-square rotate-45"></i> <hr />
+                        <hr /> <i className={`far fa-square ${styles['rotate-45']}`}></i> <i className={`far fa-square ${styles['rotate-45']}`}></i> <hr />
                     </div>
                 </div>
 
                 <div className="input-group">
-                    <input className="form-control " type="search" placeholder="Type And Hit Enter" aria-label="Search" />
+                    <input className={styles['form-control']} type="search" placeholder="Type And Hit Enter" aria-label="Search" />
                     <div className="input-group-append">
-                        <span className="input-group-text search-loop" id="basic-text1"><i className="fas fa-search text-white " aria-hidden="true"></i></span>
+                        <span className={`input-group-text ${styles['search-loop']}`} id="basic-text1"><i className="fas fa-search text-white " aria-hidden="true"></i></span>
                     </div>
                 </div>
 
