34f8124b330a2795b9aea0264b34975db5ed0f76
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-30 03:21:05 +0200

    Update RecipeCard at RecipeCatalog
    Setup css module
    Rename component function name

--- client/src/pages/RecipeCatalog/RecipesCard/RecipesCard.js
+++ client/src/pages/RecipeCatalog/RecipesCard/RecipesCard.js
@@ -1,9 +1,9 @@
 import { NavLink } from 'react-router-dom';
 
-// import styles from './SecondRecipesCard.module.css';
+import styles from './RecipesCard.module.css';
 
 
-export default function CakeList({
+export default function RecipesCard({
     recipe
 }) {
     return (
@@ -12,8 +12,8 @@ export default function CakeList({
                 <img src={recipe.image} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{recipe.title}</h5>
-                    <p className="card-text">{recipe.description}</p>
-                    <NavLink to={`/details/${recipe._id}`} className="btn btn-primary">Details</NavLink>
+                    <p className={styles['card-text']}>{recipe.description}</p>
+                    <NavLink to={`/details/${recipe._id}`} className={`${styles['btn']} ${styles['btn-primary']}`}>Details</NavLink>
                 </div>
             </div>
         </div >
