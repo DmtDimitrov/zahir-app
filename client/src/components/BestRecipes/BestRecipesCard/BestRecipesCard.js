22cdcde1765614885441cd332d07bff8ddc9ef52
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-30 13:49:08 +0200

    Update BestRecipesCard.js
    Rename class to className

--- client/src/components/BestRecipes/BestRecipesCard/BestRecipesCard.js
+++ client/src/components/BestRecipes/BestRecipesCard/BestRecipesCard.js
@@ -3,12 +3,12 @@ export default function BestRecipesCard({
 }) {
     return (
 
-        <div class="col">
-            <div class="card h-100">
-                <img src="https://zahirsweetcreations.com/wp-content/uploads/2021/07/rafaelo-03_crop_www-600x600.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Raffaello cake</h5>
-                    <p class="card-text">asdasd</p>
+        <div className="col">
+            <div className="card h-100">
+                <img src="https://zahirsweetcreations.com/wp-content/uploads/2021/07/rafaelo-03_crop_www-600x600.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Raffaello cake</h5>
+                    <p className="card-text">asdasd</p>
                 </div>
             </div>
         </div>
