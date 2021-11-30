2b0ee0d7072dcb343cc8cffa8a5a2d74e9238483
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-11-30 02:29:25 +0200

    Setup css module at Pagination component

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -1,19 +1,21 @@
 
+import styles from './Pagination.module.css';
+
 export default function Pagination() {
 
     return (
-        <nav className="pagin-nr">
+        <nav className={styles['pagin-nr']}>
             <ul className="pagination  justify-content-center">
-                <li className="page-item  arr-lf-rt">
-                    <a className="page-link" href="/#" tabIndex="-1"><i className="fas fa-arrow-left"></i> </a>
+                <li className={`${styles['page-item']} ${styles['arr-lf-rt']}`}>
+                    <a className={`${styles['page-link']} page-link`} href="/#" tabIndex="-1"><i className="fas fa-arrow-left"></i> </a>
                 </li>
-                <li className="page-item"><a className="page-link" href="/#">01</a></li>
-                <li className="page-item active">
-                    <a className="page-link" href="/#">02 <span className="sr-only">(current)</span></a>
+                <li className={styles['page-item']}><a className={`${styles['page-link']} page-link`} href="/#">01</a></li>
+                <li className={`${styles['page-item']} ${styles['active']} page-item active`}>
+                    <a className={`${styles['page-link']} page-link`} href="/#">02 <span className="sr-only">(current)</span></a>
                 </li>
-                <li className="page-item"><a className="page-link" href="/#">3</a></li>
-                <li className="page-item arr-lf-rt">
-                    <a className="page-link" href="/#"><i className="fas fa-arrow-right"></i></a>
+                <li className={styles['page-item']}><a className={`${styles['page-link']} page-link`} href="/#">3</a></li>
+                <li className={`${styles['page-item']} ${styles['arr-lf-rt']}`}>
+                    <a className={`${styles['page-link']} page-link`} href="/#"><i className="fas fa-arrow-right"></i></a>
                 </li>
             </ul>
         </nav>
