fcad616aca211d4757747651ecef1f1199e89de6
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-11 00:11:18 +0200

    Setup Recent Recipes Component

--- client/src/components/RecentRecipes/RecentRecipeCard/RecentRecipeCard.js
+++ client/src/components/RecentRecipes/RecentRecipeCard/RecentRecipeCard.js
@@ -1,20 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
 import styles from './RecentRecipeCard.module.css';
 
-export default function RecentRecipeCard(recipe) {
+export default function RecentRecipeCard({ recipe }) {
 
     return (
         <>
             <div className={`row ${styles['ltl-blog']}`}>
                 <div className="col-4">
-                    <img src={recipe.image} alt="" className={`${styles['recent-img']} img-fluid`} />
+                    <NavLink to={`/recipes/details/${recipe._id}`}>
+                        <img src={recipe?.image} alt="" className={`${styles['recent-img']} img-fluid`} />
+                    </NavLink>
                 </div>
                 <div className={`col-8 ${styles['max-award']}`}>
-                    <p> {recipe.title}</p>
+                    <p> {recipe?.title}</p>
                     <i className={`far fa-heart ${styles['blog-icon']}`}></i>
-                    <span> 79 </span>
+                    <span> {recipe?.likes.length} </span>
                     <span className={styles['blog-vl']}> | </span>
                     <i className={`far fa-comment ${styles['blog-icon']}`}></i>
-                    <span> 53 </span>
+                    <span> {recipe?.comments.length} </span>
                 </div>
             </div>
             <hr />
