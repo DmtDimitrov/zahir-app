0d263ef333258bad7ee6aa0f568fe5025d7fb439
Dimitar Dimitrov <dmt.dimitrov@gmail.com>
2021-12-11 12:51:49 +0200

    Update index.js
    update local storage key name for user and recipe

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,5 @@
 export const HOST = 'http://localhost:3050/api';
 export const USERNAME = 'username';
 export const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-export const USER_TOKEN = 'user';
+export const USER_LOCAL_STORAGE_KEY_NAME = 'user';
+export const RECIPE_LOCAL_STORAGE_KEY_NAME = 'recipe';
