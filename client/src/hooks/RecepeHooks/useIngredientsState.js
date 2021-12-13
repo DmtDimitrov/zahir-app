1aa7608e1f7b4453dbbea9a2006b23d76a611ee5
Dimitar Dimitrov <dmt.dimitrov@gmail.com>
2021-12-13 18:52:05 +0200

    Update useIngredientsState.js

--- client/src/hooks/RecepeHooks/useIngredientsState.js
+++ client/src/hooks/RecepeHooks/useIngredientsState.js
@@ -1,30 +1,101 @@
 import { useState, useEffect } from "react";
 
-// import * as recipeService from '../../services/recipeService';
-// import { useRecipeContext } from '../../contexts/RecipeContext';
+
 
 export const useIngredientsState = () => {
     const [ingredientInputs, setIngredientInputs] = useState([]);
-    // const { setRecipeContext } = useRecipeContext();
 
-    useEffect(() => {
-        recipeService.getOne(recipeId)
-            .then(result => {
-                setRecipe(result);
-                
-            })
-            .catch(error => {
-                console.log(error);
-            })
-            .finally(result => {
-                setRecipeContext(result)
-            })
-    }, [recipeId, setRecipeContext]);
+
+    const oldStateIsValid = () => {
+        if (ingredientInputs.length === 0) {
+            return true;
+        };
+
+        const someEmpty = ingredientInputs.some((item) =>
+            item.Ingredient === ''
+            || item.Unit === ''
+            || item.Quantity === ''
+        );
+
+        if (someEmpty) {
+            ingredientInputs.map((item, index) => {
+                const allOldState = [...ingredientInputs];
+
+                if (ingredientInputs[index].Ingredient === '') {
+                    allOldState[index].errors.Ingredient = 'Ingredient is required';
+                }
+
+                if (ingredientInputs[index].Unit === '') {
+                    allOldState[index].errors.Unit = 'Unit is required';
+                }
+
+                if (ingredientInputs[index].Quantity === '') {
+                    allOldState[index].errors.Quantity = 'Quantity is required';
+                }
+
+                return setIngredientInputs(allOldState);
+            });
+        }
+
+        return !someEmpty;
+    }
+
+
+    const addIngredientHandler = (e) => {
+        e.preventDefault();
+        const ingredientInputState = {
+            Ingredient: '',
+            Unit: '',
+            Quantity: '',
+            errors: {
+                Ingredient: null,
+                Unit: null,
+                Quantity: null,
+            }
+        };
+
+        if (oldStateIsValid()) {
+            setIngredientInputs(oldState => [...oldState, ingredientInputState]);
+        }
+    };
+
+    const onChangeIngredients = (index, event) => {
+        event.preventDefault();
+        event.persist();
+
+        setIngredientInputs(oldState => {
+            return oldState.map((item, i) => {
+
+                if (i !== index) {
+                    return item;
+                };
+
+                return {
+                    ...item,
+                    [event.target.name]: event.target.value,
+                    errors: {
+                        ...item.errors,
+                        [event.target.name]: event.target.value.length > 0 ? null : [event.target.name] + ' Is required',
+                    }
+                };
+            });
+        });
+    };
+
+
+    const removeIngredientInputHandler = (e, index) => {
+        e.preventDefault();
+
+        setIngredientInputs(oldState => oldState.filter((item) => item !== oldState[index]))
+    }
+
 
     return [
-        recipe,
-        setRecipe
+        ingredientInputs,
+        addIngredientHandler,
+        onChangeIngredients,
+        removeIngredientInputHandler, 
+        setIngredientInputs
     ]
 };
 
-// export default useRecipeState;
