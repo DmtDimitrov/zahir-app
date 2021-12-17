64c2dada9d631050c6517cf8ee3a608d926a333a
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-17 04:29:59 +0200

    Update Recipe bar

--- client/src/components/Recipes/Bar/TopRecipes/TopRecipes.js
+++ client/src/components/Recipes/Bar/TopRecipes/TopRecipes.js
@@ -1,20 +1,24 @@
-import styles from './RecentRecipes.module.css';
-import RecentRecipeCard from './RecentRecipeCard';
-import { useRecipesState } from '../../../hooks/RecepeHooks/useRecipesState'
+import styles from '../css/RecipesBar.module.css';
+import RecentRecipeCard from '../RecentRecipeCard';
+import { useRecipesState } from '../../../../hooks/RecepeHooks/useRecipesState';
 
-export default function RecentRecipes() {
+export default function TopRecipes() {
     const [recipes] = useRecipesState();
-    console.log(recipes);
+
+    let topRecipes = recipes?.sort((a,b) => {
+        return b.likes.length - a.likes.length
+    })
+
     return (
         <div className={`row ${styles['blog-row-padd']}`}>
             <div className={`col-12 ${styles['side-bar-bg']}`}>
                 <div className={`${styles['our-header']} text-center`}>
-                    <span>recent post</span>
+                    <span>top recipes</span>
                     <div className="text-center">
                         <hr /><i className={`far fa-square ${styles['rotate-45']}`}></i> <i className={`far fa-square ${styles['rotate-45']}`}></i><hr />
                     </div>
                 </div>
-                {recipes && recipes.slice(-3).map(x => <RecentRecipeCard key={x._id} recipe={x} />)}
+                {topRecipes && topRecipes.slice(0,3).map(x => <RecentRecipeCard key={x._id} recipe={x} />)}
 
 
             </div>
