e1ddedf20f6ab0bc5d509db9dfca313846782260
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-17 16:43:41 +0200

    Reorganize components folders

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,10 @@
 import './Home.css';
 
 import Page from '../Page';
-import Carousel from '../../components/Carousel';
-import PopularRecipes from '../../components/PopularRecipes';
-import LatestRecipes from '../../components/LatestRecipes';
-import BestRecipes from '../../components/BestRecipes';
+import Carousel from '../../components/Recipes/home/Carousel';
+import PopularRecipes from '../../components/Recipes/home/PopularRecipes';
+import LatestRecipes from '../../components/Recipes/home/LatestRecipes';
+import BestRecipes from '../../components/Recipes/home/BestRecipes';
 
 export default function Home() {
     return (
