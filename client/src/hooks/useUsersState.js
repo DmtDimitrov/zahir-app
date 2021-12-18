4c7a2f76318fed0468ac93272a133bf1c1b5eadb
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-18 03:09:33 +0200

    Create useUsersState.js

---
+++ client/src/hooks/useUsersState.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+import * as authService from '../services/authService';
+
+export const useUsersState = () => {
+    const [users, setUsers] = useState(null);
+
+
+    useEffect(() => {
+        authService.getAll()
+            .then(result => {
+                console.log(result);
+                setUsers(result)
+            })
+            .catch(err => {
+                console.log('Users catalog: err');
+                console.log(err);
+            })
+    }, []);
+
+    return [
+        users,
+        setUsers
+    ]
+};
+
+
