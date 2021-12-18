2e1ef1440735bf20cb64ade60953db53367ecfb1
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-18 03:10:22 +0200

    Create Chefs card component

---
+++ client/src/pages/Chefs/ChefsCard/ChefsCard.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+import styles from './ChefsCard.module.css';
+
+
+export default function ChefsCard({
+    user,
+}) {
+    
+
+    return (
+        <div className="col-sm-6 col-lg-4 mb-3">
+            <div className={`card h-100 ${styles['card-container']}`}>
+                <Link to={`/user/details/${user?._id}`}>
+                    <img src={user?.image ? user.image: "zahir/default-profile.jpg"} className="card-img-top" alt="..." />
+                </Link>
+                <div className={`${styles['card-body']} ${styles['post-info']} card-body`}>
+                    <h5 className="card-title">{user && `${user.firstName} ${user.lastName}`}</h5>
+                    {/* {user?.recipes &&
+                        <p className={styles['card-text']}>
+                            <span className={styles['info-over']}>
+                                <i className="far fa-user"></i>
+                                By {`${recipe.ownerId.firstName} ${recipe.ownerId.lastName}`}
+                            </span>
+                          
+                        </p>} */}
+                </div>
+            </div>
+        </div >
+    );
+}
+
+
+
+
