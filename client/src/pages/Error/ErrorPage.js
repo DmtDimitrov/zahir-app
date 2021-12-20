41c39d12a876aa48b9d3c96b10f432deb1c85c7a
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-20 11:24:36 +0200

    Update Error page

--- client/src/pages/Error/ErrorPage.js
+++ client/src/pages/Error/ErrorPage.js
@@ -1,14 +1,52 @@
+import styles from './ErrorPage.module.css';
 
+import Page from '../Page';
+import Subheader from '../../components/Subheader';
+import SearchBar from '../../components/SearchBar';
+import CategoriesBar from '../../components/CategoriesBar';
+import RecentRecipes from '../../components/Recipes/Bar/RecentRecipes';
+import TopRecipes from '../../components/Recipes/Bar/TopRecipes';
 
 
 export default function ErrorPage({
     children
 }) {
     return (
-        <section id="catalog-page" className="error-page">
-            <h1>Page Not Found</h1>
+        <Page>
+            <Subheader
+                title="Error Page"
+            />
 
-            {children && <p className="no-articles">{children}</p>}
-        </section>
+
+
+
+
+
+            <div className={`${styles['main-container']} ${styles['blog-container']}`}>
+                <div className={styles['inside-container']}>
+                    <div className="row">
+                        <div className="col-lg-8 " >
+                            <h1>Page Not Found</h1>
+
+                            {children && <p className="no-articles">{children}</p>}
+                        </div>
+
+                        <div className={`col-lg-4 ${styles['side-bar-colon']}`} >
+
+                            <SearchBar />
+
+                            <CategoriesBar />
+
+                            <RecentRecipes />
+
+                            <TopRecipes />
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </Page>
     );
 }
