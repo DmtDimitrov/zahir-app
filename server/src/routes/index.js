10d7f74053bd64cb63bd2f28ad5fdaf8b1f70a16
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-21 14:22:06 +0200

    Add comment controller
    Add comment controller

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import userController from '../controllers/userController.js';
 import recipeController from '../controllers/recipeController.js';
 import categoryController from '../controllers/categoryController.js';
+import commentController from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.use('/users', userController);
 router.use('/recipes', recipeController);
 router.use('/category', categoryController);
+router.use('/comments', commentController);
 
 export default router;
