85aa8ca1018ac9103e07c11ae4e9b80add216368
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-21 22:03:34 +0200

    Update Notification.js
    Remove Toast Header

--- client/src/components/common/Notification/Notification.js
+++ client/src/components/common/Notification/Notification.js
@@ -15,11 +15,11 @@ const Notification = () => {
     return (
         <ToastContainer className="p-3" position="middle-end">
             <Toast className="d-inline-block m-1" bg={notification.type} onClose={hideNotification}>
-                <Toast.Header>
+                {/* <Toast.Header>
                     <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
                     <strong className="me-auto">{notification.title}</strong>
                     <small>{ }</small>
-                </Toast.Header>
+                </Toast.Header> */}
                 <Toast.Body >
                     {notification.message}
                 </Toast.Body>
