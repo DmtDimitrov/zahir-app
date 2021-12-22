1bf80a24ade4951f794e328bcf03cb864a9102c7
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-22 21:32:57 +0200

    Update NavbarMenu.js

--- client/src/components/NavbarMenu/NavbarMenu.js
+++ client/src/components/NavbarMenu/NavbarMenu.js
@@ -6,9 +6,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 const NavbarMenu = () => {
 
@@ -69,15 +66,6 @@ const NavbarMenu = () => {
 
                     </Nav>
 
-                    <Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
 
                 </Navbar.Collapse>
             </Container>
