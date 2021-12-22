dd74401e31980cf8b659e9f91af1968f512e7af6
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-22 22:56:48 +0200

    Update CategoriesLink.js

--- client/src/components/CategoriesBar/CategoriesLink/CategoriesLink.js
+++ client/src/components/CategoriesBar/CategoriesLink/CategoriesLink.js
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
+
+
 import styles from './CategoriesLink.module.css';
 
 export default function CategoriesLink({
     category,
 }) {
+
     return (
         <>
             <div className={styles['categories-link']}>
-                <a href="/#"> <i className="fas fa-angle-right"></i> {category?.name}</a>
+                <Link to={`/category/${category?.name}`} ><i className="fas fa-angle-right"></i> {category?.name}</Link>
+                {/* <a href="/#"> <i className="fas fa-angle-right"></i> {category?.name}</a> */}
             </div>
             <hr />
         </>
