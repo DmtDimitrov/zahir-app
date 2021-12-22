755d34700fbaede93614eac80a64c84fa1ad81f8
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-12-23 01:20:19 +0200

    Remove Cook button

--- client/src/pages/Recipe/Details/DetailsContentCard/DetailsContentCard.js
+++ client/src/pages/Recipe/Details/DetailsContentCard/DetailsContentCard.js
@@ -42,7 +42,6 @@ export default function RecipeDetailsContentCard({
                     </Button>
                 </Modal.Footer>
             </Modal>
-            <button>Cook</button>
         </>
     );
 
